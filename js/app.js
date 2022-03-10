a29f90f0d32bcb290c90d68379b1c697813c7501
Your Name <you@example.com>
2022-03-10 23:53:11 +0200

    comment section user is showing

--- js/app.js
+++ js/app.js
@@ -122,7 +122,7 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
                       ${post.comments?.text}
                     </small>
